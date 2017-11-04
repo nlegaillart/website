2d21bda141871cfefca46f33317cf48681977af7
n <n@tourmentine.com>
2017-11-04 23:38:41 +0100

    https://gethead.info/

--- books/header.php
+++ books/header.php
@@ -3,20 +3,20 @@
 <html manifest="cache.manifest" lang="fr">
 <head>
 	<title>Foire aux livres</title>
-        <meta charset="utf-8" />
+	<meta charset="utf-8" />
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
-i	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="keywords" content="cv, curriculum vitae, emploi, informatique, internet,technicien, nicolas, gaillart, gaillard, legaillart, legaillard, programmeur, analyste,developpeur, developpement, administrateur, systeme, reseaux, linux, unix, bsd, dut, bac+2, demandeur" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="keywords" content="cv, curriculum vitae, emploi, informatique, internet,technicien, nicolas, gaillart, gaillard, legaillart, legaillard, programmeur, analyste,developpeur, developpement, administrateur, systeme, reseaux, linux, unix, bsd, dut, bac+2, demandeur" />
 	<meta name="description" content="Don de livres auxquels j'ai trouv&eacute; un &eacute;quivalent num&eacute;rique."/>
-        <meta content="text/html; charset=UTF-8" http-equiv="Content-Type" />
-        <meta name="revisit-after" content="10 days" />
-        <link href="/css.css" rel="stylesheet" type="text/css" media="screen" />
-        <link href="/p.css" rel="stylesheet" type="text/css" media="print" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="icon" type="image/gif" href="/animated_favicon1.gif" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <meta name="verify-v1" content="ObmA5u+NGovrfXvZB62T7q2omVOIX4Fl3mWdGuQkPH8=" />
-        <meta name="google-site-verification" content="Qbt4lmsHxpvnKaic2UGAwb4P9es0FEADF1jLk2uo_cs" />
+	<meta content="text/html; charset=UTF-8" http-equiv="Content-Type" />
+	<meta name="revisit-after" content="10 days" />
+	<link href="/css.css" rel="stylesheet" type="text/css" media="screen" />
+	<link href="/p.css" rel="stylesheet" type="text/css" media="print" />
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<link rel="icon" type="image/gif" href="/animated_favicon1.gif" />
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+	<meta name="verify-v1" content="ObmA5u+NGovrfXvZB62T7q2omVOIX4Fl3mWdGuQkPH8=" />
+	<meta name="google-site-verification" content="Qbt4lmsHxpvnKaic2UGAwb4P9es0FEADF1jLk2uo_cs" />
 
 	<script src="/js/jquery.min.js"></script>
 	<script src="/js/js.js"></script>
