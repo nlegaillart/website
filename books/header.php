cc6f8225824d65e65ddd9c57b6fe2f9afd75b9a7
n <n@tourmentine.com>
2017-11-04 21:31:39 +0100

    name first

--- books/header.php
+++ books/header.php
@@ -4,17 +4,16 @@
 <head>
 	<title>Foire aux livres</title>
         <meta charset="utf-8" />
-	<meta content="Site personnel de Nicolas Le Gaillart: coordonn&eacute;es, CV, livres, citations diverses, cuisine" name="description" />
-        <meta content="cv, curriculum vitae, emploi, informatique, internet,technicien, nicolas, gaillart, gaillard, legaillart, legaillard, programmeur, analyste,developpeur, developpement, administrateur, systeme, reseaux, linux, unix, bsd, dut, bac+2, demandeur" name="keywords" />
+        <meta name="keywords" content="cv, curriculum vitae, emploi, informatique, internet,technicien, nicolas, gaillart, gaillard, legaillart, legaillard, programmeur, analyste,developpeur, developpement, administrateur, systeme, reseaux, linux, unix, bsd, dut, bac+2, demandeur" />
 	<meta name="description" content="Don de livres auxquels j'ai trouv&eacute; un &eacute;quivalent num&eacute;rique."/>
         <meta content="text/html; charset=UTF-8" http-equiv="Content-Type" />
-        <meta content="10 days" name="revisit-after" />
+        <meta name="revisit-after" content="10 days" />
         <link href="/css.css" rel="stylesheet" type="text/css" media="screen" />
         <link href="/p.css" rel="stylesheet" type="text/css" media="print" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="icon" type="image/gif" href="/animated_favicon1.gif" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <meta content="ObmA5u+NGovrfXvZB62T7q2omVOIX4Fl3mWdGuQkPH8=" name="verify-v1" />
+        <meta name="verify-v1" content="ObmA5u+NGovrfXvZB62T7q2omVOIX4Fl3mWdGuQkPH8=" />
         <meta name="google-site-verification" content="Qbt4lmsHxpvnKaic2UGAwb4P9es0FEADF1jLk2uo_cs" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
